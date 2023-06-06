6a3ec9b0e2d0218612679a28f9ef84316c2772eb
xxwonderwallxx <vika010501@gmail.com>
2023-06-07 00:46:31 +0300

    download image function

--- src/components/InfoDialog/InfoDialog.jsx
+++ src/components/InfoDialog/InfoDialog.jsx
@@ -8,13 +8,27 @@ import {
     Button
 } from '@mui/material';
 
-export default function InfoDialog({ 
-    isOpen, 
-    title, 
+export default function InfoDialog({
+    isOpen,
+    title,
     sourceUrl,
-    description, 
-    dateCreated, 
+    description,
+    dateCreated,
     onIsOpenChange }) {
+
+    async function downloadImage(imageSrc) {
+
+        const image = await fetch(imageSrc);
+        const imageBlog = await image.blob();
+        const imageURL = URL.createObjectURL(imageBlog);
+
+        const link = document.createElement('a');
+        link.href = imageURL;
+        link.download = title + '_' + Date.now();
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 
     return (
 
@@ -32,10 +46,11 @@ export default function InfoDialog({
                 <DialogActions>
                     <Typography>{dateCreated.substring(0, 9)}</Typography>
                     <Button href='https://images.nasa.gov/'>Learn More</Button>
+                    <Button onClick={() => downloadImage(sourceUrl)}>Download</Button>
                     <Button onClick={() => onIsOpenChange(false)}>Close</Button>
                 </DialogActions>
             </Dialog>
             :
-            <div style={{display: 'none'}}></div>
+            <div style={{ display: 'none' }}></div>
     )
 }
